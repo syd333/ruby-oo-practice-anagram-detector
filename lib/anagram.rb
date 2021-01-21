42e9b807ceffe2d512f2b2d5a808dfaf16118735
syd333 <sydmiranda@icloud.com>
2021-01-20 21:44:52 -0500

    commit tree f2e6859f60468921c4afa5b8eb23dd525de2c8c1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+
+
+attr_accessor :word
+
+def initialize(word)
+    @word = word
+end
+
+def match(anagrams)
+    anagrams.select do |anagram| 
+       (anagram.split("")).sort == (word.split("")).sort
+    end
+end
+
+end
